458afecee13a8dcbc595bdacbc94245cb54e0579
Carlos Valle <carlos.m.valle@gmail.com>
2023-05-03 11:27:47 -0400

    feat(stepper): new options and statuts tag fix
    New options to:
    - Hide footer
    - Hide validation icon
    - Custom save button

--- libs/layouts/src/lib/stepper/stepper.component.ts
+++ libs/layouts/src/lib/stepper/stepper.component.ts
@@ -20,6 +20,10 @@ export class SLStepperComponent extends SdsStepper {
   @Input() expirationDate: string;
   @Input() reviewMode = false;
   @Input() hideSideNav = true;
+  @Input() hideFooter = false;
+  @Input() customSave = false;
+  @Input() hideValidationIconForIDs = [];
+
   @Output() versionChange = new EventEmitter<any>();
   @Output() previousClick = new EventEmitter<any>();
   @Output() nextClick = new EventEmitter<any>();
@@ -34,5 +38,9 @@ export class SLStepperComponent extends SdsStepper {
     cdr: ChangeDetectorRef
   ) {
     super(router, activatedRoute, cdr);
+  }
+
+  showValidationIcon(stepID: string){
+    return !this.hideValidationIconForIDs.includes(stepID);
   }
 }
