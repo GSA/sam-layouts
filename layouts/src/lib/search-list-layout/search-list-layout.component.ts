ef4f53dadf8684d0fa7c4b86b3d720db171c790b
Pavani Kondapalli <pavani.kondapalli@gsa.gov>
2024-11-07 01:09:57 -0500

    IAEMOD-36235 Enable custom search message

--- layouts/src/lib/search-list-layout/search-list-layout.component.ts
+++ layouts/src/lib/search-list-layout/search-list-layout.component.ts
@@ -67,6 +67,12 @@ export class SearchListLayoutComponent implements OnInit {
 
   @Input() enableApiCall: boolean = true;
 
+  @Input() showCustomSearchMessage = false;
+
+  @Input() searchMessageHeader = "Select a category above to view results.";
+
+  @Input() searchMessageText = "Choose your filters and run your report to begin.";
+
   @Output() sortFieldChange = new EventEmitter<string>();
 
   /**
